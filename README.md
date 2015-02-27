450262de0bd4b2196a1f054176b0f47169efc251
Ernest Kirstein <glank314@gmail.com>
2015-02-27 11:37:54 -0500

    Added more documentation.

--- README.md
+++ README.md
@@ -9,11 +9,25 @@ It currently handels collision detection and simple bounce rules.
 Right now, I'm working on documenting all the code.
 
 To view the documentation, you'll need to build the project
-(it's all javadoc-generated). Just run `make` from the project 
+(it's all javadoc-generated). Just run `make docs` from the project 
 directory to build the jars and generate the documentation.
 
-You can then run any of the 3 examples:
+You can then run any of the 4 examples:
 
     ./runExample.sh BounceEngineExample
     ./runExample.sh BallChase
     ./runExample.sh Pong
+    ./runExample.sh Jump
+
+I appologize that the project is only set up for linux at the
+moment. Making it Windows (& Mac ?) friendly is on the TODO list:
+
+TODO
+----
+
+* Document the existing code.
+* Create Install/Getting-Started Tutorial
+    * Linux
+    * Windows
+    * Mac?
+* Release Binary
--- org/pac/games/BounceGameWindow.java
+++ org/pac/games/BounceGameWindow.java
@@ -10,6 +10,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.LinkedList;
 
+
+/**
+ * A window that can serve as a base class for games which
+ * use the BounceEngine (a simple physics engine).
+ */
 public class BounceGameWindow extends SimpleGameWindow
     implements CollisionListener{
     private static final long serialVersionUID = 34587892834L;
@@ -39,37 +44,80 @@ public class BounceGameWindow extends SimpleGameWindow
     public BounceGameWindow(){
         this(new BounceEngine());
     }
+
+    /**
+     * Add a BouncerSprite to the physics engine and the screen.
+     */
     public void addSprite(BouncerSprite sprite){
         engine.add(sprite);
         sprites.add(sprite);
     }
+    /**
+     * Remove a BouncerSprite from the physics engine and the screen.
+     */
     public boolean removeSprite(BouncerSprite sprite){
         engine.remove(sprite);
         return sprites.remove(sprite);
     }
+    /**
+     * Add a MovingRectangle to the collision detection part of the
+     * physics engine.
+     */
     public void add(MovingRectangle rectangle){
         engine.add(rectangle);
     }
+    /**
+     * Remove a MovingRectangle from the collision detection part of the
+     * physics engine.
+     */
     public void remove(MovingRectangle rectangle){
         engine.remove(rectangle);
     }
+    /**
+     * Remove all sprites and rectangles from the physics engine and the
+     * screen.
+     */
     public void clear(){
         sprites.clear();
         engine.clear();
     }
+    /**
+     * Get the underlying BounceEngine (physics engine).
+     */
     public BounceEngine getEngine(){
         return engine;
     }
     //bounce engine wrappers
+    /**
+     * Tell the physics engine to telleport 'b' to 'location'.
+     */
     public void teleport(Bouncer b, MovingRectangle location){
         engine.teleport(b,location);
     }
+    /**
+     * Add an invisible 1pxl border to the physics engine.
+     */
     public void addBounceBorder(double x, double y, double width, double height){
         engine.addBounceBorder(x,y,width,height);
     }
 
     //optional overloadables
+    /**
+     * A method to override (optionally) - this can update the game's state every
+     * couple of seconds.
+     * @see GameUpdater
+     */
     public void update(double dt){}
+    /**
+     * A method to override (optionally) - this renders additional graphics on top of
+     * the background and sprites (which are drawn automatically).
+     * @see GamePainter
+     */
     public void paint(Graphics g){}
+    /**
+     * A method to override (optionally) - this method is called whenever the
+     * physics engine detects a collision. 
+     * @see CollisionListener
+     */
     public void handelCollision(CollisionInstance instance){}
 }
